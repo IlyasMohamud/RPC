a0389087bf0b125d9f865ed35f5132d48a41e397
Ilyas <ilyasmo_1999@hotmail.co.uk>
2023-06-15 19:37:19 +0200

    added a function that checks for winner, have not tested it yet

--- RPC.py
+++ RPC.py
@@ -1,5 +1,5 @@
 import random
-choices = ["Rock", "Paper", "Scissors"]
+choices = ["rock", "paper", "scissors"]
 
 def computer():
     x = random.randint(1,3)
@@ -7,4 +7,44 @@ def computer():
     return choice
 
 
-print (computer())
+def user():
+    while True:
+        x = input("Choose between either Rock, Paper or Scissors ")
+        choice = x.lower()
+        if choice in choices:
+            return choice
+        
+        print("Invalid input")
+
+def checkWinner(user, comp):#very redundant try to make shorter, maybe make choices into numbers and check?
+    if user == comp:
+        print("It was a draw")
+        return 0
+    if user == "scissors" & comp == "paper":
+         print("You won")
+         return 1
+    if user == "rock" & comp == "scissors":
+        print("You won")
+        return 1
+    if user == "paper" & comp == "rock":
+        print("You won")
+        return 1
+
+    else:
+        print("You lost")
+        return 0
+
+
+
+def RPC(): 
+    print("Welcome to Rock Paper Scissors\n")
+    
+
+
+
+
+
+if __name__ == "__main__":
+
+    RPC()
+
